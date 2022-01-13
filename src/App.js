f092ad513182005fa894c2ded95abaf50ee824f9
brianjtarte <brianjtarte@gmail.com>
2022-01-12 21:08:59 -0800

    installed bootstrap

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './Header';
 import Footer from './Footer';
 import Main from './Main';
 import axios from 'axios';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends React.Component{
   constructor(
